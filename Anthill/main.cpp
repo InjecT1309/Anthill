bdd690925b8bcb2229631eae1803c876c5857fd7
shahar23 <paweljossa@gmail.com>
2017-06-09 15:35:13 +0200

    removed loop for testing

--- Anthill/main.cpp
+++ Anthill/main.cpp
@@ -14,14 +14,5 @@ int main()
 {
     Map2D Map;
 
-    for(int y=1; y<20; y++)
-    {
-        for(int x=1; x<20; x++)
-        {
-            cout << Map.WhatIsHere(x, y).m_Fill << " ";
-        }
-        cout << endl;
-    }
-
     return 0;
 }
